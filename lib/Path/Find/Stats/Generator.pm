ce211a22149515dab76bab39ac9cbe174dd85a26
Carla Cummins <cc21@sanger.ac.uk>
2013-10-30 10:19:40 +0000

    rnaseqfind stats

--- lib/Path/Find/Stats/Generator.pm
+++ lib/Path/Find/Stats/Generator.pm
@@ -257,10 +257,85 @@ sub assemblyfind {
     }
 }
 
+sub rnaseqfind {
+    my ($self) = @_;
+    my @lanes = @{ $self->lane_hashes };
+
+    # set up headers and info to retrieve for each row
+    my @headers = (
+        'Study ID',
+        'Sample',
+        'Lane Name',
+        'Cycles',
+        'Reads',
+        'Bases',
+        'Map Type',
+        'Reference',
+        'Reference Size',
+        'Mapper',
+        'Mapstats ID',
+        'Mapped %',
+        'Paired %',
+        'Mean Insert Size',
+        'Depth of Coverage',
+        'Depth of Coverage sd',
+        'Genome Covered (% >= 1X)',
+        'Genome Covered (% >= 5X)',
+        'Genome Covered (% >= 10X)',
+        'Genome Covered (% >= 50X)',
+        'Genome Covered (% >= 100X)',
+    );
+
+    my @columns = (
+        'study_id',           'sample',
+        'lanename',           'cycles',
+        'reads',              'bases',
+        'map_type',           'reference',
+        'reference_size',     'mapper',
+        'mapstats_id',        'mapped_perc',
+        'paired_perc',        'mean_insert_size',
+        'depth_of_coverage',  'depth_of_coverage_sd',
+        'genome_covered_1x',  'genome_covered_5x',
+        'genome_covered_10x', 'genome_covered_50x',
+        'genome_covered_100x'
+    );
+
+    # set up output file handle
+    open( OUT, ">", $self->output );
+
+    #output headers
+    my $header_line = join( ",", @headers );
+    print OUT "$header_line\n";
+
+    #loop through lanes and print info to file
+    my $vrtrack = $self->vrtrack;
+    foreach my $l_h (@lanes) {
+		my $l = $l_h->{lane};
+        my $mapstat = $self->_select_mapstat( $l->mappings_excluding_qc );
+        my $row     = Path::Find::Stats::Row->new(
+            lane     => $l,
+            mapstats => $mapstat,
+            vrtrack  => $vrtrack
+        );
+
+        my @info;
+        foreach my $c (@columns) {
+            my $i = defined( $row->$c ) ? $row->$c : "NA";
+            push( @info, $i );
+        }
+        my $row_joined = join( ',', @info );
+        print OUT "$row_joined\n";
+    }
+}
+
+
 sub _select_mapstat {
     my ( $self, $mapstats ) = @_;
 
     my @sorted_mapstats = sort { $a->row_id <=> $b->row_id } @{$mapstats};
+
+	print Dumper \@sorted_mapstats;
+	
     return pop(@sorted_mapstats);
 }
 
